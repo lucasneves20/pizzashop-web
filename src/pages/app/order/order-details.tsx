13e753a6bfa432e494fb6a0a9858e48a1f8c5b76
webMasterAlumni <heraldofdevs@gmail.com>
2024-06-10 13:33:19 -0300

    ui criada, só conexão com API

--- src/pages/app/order/order-details.tsx
+++ src/pages/app/order/order-details.tsx
@@ -4,11 +4,17 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
-interface OrderDetailsProps {}
-
-export function OrderDetails({}: OrderDetailsProps) {
+export function OrderDetails() {
   return (
     <DialogContent>
       <DialogHeader>
@@ -33,21 +39,70 @@ export function OrderDetails({}: OrderDetailsProps) {
                 </div>
               </TableCell>
             </TableRow>
+
             <TableRow>
               <TableCell className="text-muted-foreground">Cliente</TableCell>
               <TableCell className="flex justify-end">Lucas Neves</TableCell>
             </TableRow>
+
             <TableRow>
               <TableCell className="text-muted-foreground">Telefone</TableCell>
               <TableCell className="flex justify-end">
                 (16) 9 9999-9999
               </TableCell>
             </TableRow>
+
             <TableRow>
               <TableCell className="text-muted-foreground">E-mail</TableCell>
-              <TableCell className="flex justify-end">lu</TableCell>
+              <TableCell className="flex justify-end">
+                lucasneves@test.com
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">
+                Realizado há
+              </TableCell>
+              <TableCell className="flex justify-end">Há 3 min</TableCell>
             </TableRow>
           </TableBody>
+        </Table>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-right">Produto</TableHead>
+              <TableHead className="text-right">Qnd.</TableHead>
+              <TableHead className="text-right">Preço</TableHead>
+              <TableHead className="text-right">Subtotal</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="text-right">
+                Pizza Pepperoni Família
+              </TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 69,90</TableCell>
+              <TableCell className="text-right">R$ 139,80</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-right">
+                Pizza Mussarela Família
+              </TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 59,90</TableCell>
+              <TableCell className="text-right">R$ 119,80</TableCell>
+            </TableRow>
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total do pedido</TableCell>
+              <TableCell className="text-right font-medium">
+                R$ 259,60
+              </TableCell>
+            </TableRow>
+          </TableFooter>
         </Table>
       </div>
     </DialogContent>
